53cb9ee56dad6042f0f95222444632e1235ba1e2
Jhosbel <jhosbel.vargas@gmail.com>
2024-03-11 12:00:44 -0600

    styling the calendar and fix the add list and render in live exercise

--- src/app/feeding/page.tsx
+++ src/app/feeding/page.tsx
@@ -1,13 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import Modal from "@/components/Modal";
-import FullCalendar from "@fullcalendar/react";
 import { useEffect, useState } from "react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import allLocales from "@fullcalendar/core/locales-all";
-import { DateSelectArg, EventInput } from "@fullcalendar/core";
 import {
   createCalendarData,
   getCalendarData,
@@ -15,34 +9,20 @@ import {
   getTrainingListOne,
 } from "@/api/training";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import { format, subHours, startOfMonth } from "date-fns";
+import { format } from "date-fns";
 
 export default function Feeding() {
   const [value, onChange] = useState<any>(new Date());
-  /* const {calendarData} = useTraining() */
   const [open, setOpen] = useState<boolean>(false);
   const [data, setData] = useState<any>();
   const [traininigList, setTrainingList] = useState<any>();
   const [calendarData, setCalendarData] = useState<any>();
-  const [titleList, setTitleList] = useState<string>("");
   const [trainingListId, setTrainingListId] = useState<any>({
     id: "",
     title: "",
   });
 
-  function createEventId() {
-    let eventGuid = 0;
-    return String(eventGuid++);
-  }
-
   useEffect(() => {
-    /* getTrainingListOne(trainingListId)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setTitleList(data.title);
-      }); */
     getTrainingList()
       .then((res) => res.json())
       .then((data) => setTrainingList(data));
@@ -52,7 +32,7 @@ export default function Feeding() {
   }, [trainingListId]);
 
   const handleModalOpen = async (clickedExercise: any) => {
-    let id = clickedExercise.event.id;
+    let id = clickedExercise.id;
     const res = await getTrainingListOne(id);
     const data = await res.json();
     setData(data);
@@ -63,109 +43,60 @@ export default function Feeding() {
     setOpen(false);
   };
 
-  /* const fecha = new Date().toISOString().replace(/T.*$/, "");
-  console.log(fecha);
-  console.log(data);
-  console.log(titleList);
-  console.log(traininigList);
-  console.log(trainingListId);
-  console.log(calendarData); */
+  const handleDayClick = async (value: any) => {
+    const fechaFormateada = format(value, "yyyy,MM,dd");
+    if (!trainingListId || trainingListId.id === "") return;
 
-  const handleDateSelect = async (selectInfo: DateSelectArg) => {
-    const { startStr } = selectInfo;
-    let calendarApi = selectInfo.view.calendar;
-    if (!trainingListId) return;
-    if (trainingListId.id === "") {
-      return alert("ID no proporcionado");
-    }
+    const confirmCreateEvent = window.confirm("¿Deseas crear el evento?");
+    if (!confirmCreateEvent) return; // Si el usuario cancela, no se crea el evento
+
     await createCalendarData({
       id: trainingListId.id,
-      start: startStr,
+      start: fechaFormateada,
       title: trainingListId.title,
     });
-    if (trainingListId) {
-      /* const newCalendaData = {...calendarData, event}
-      setCalendarData(newCalendaData) */
-      calendarApi.addEvent({
-        id: trainingListId.id,
-        title: trainingListId.title,
-        start: selectInfo.startStr,
+
+    setTrainingListId({ id: "", title: "" });
+
+    const updatedCalendarData = await getCalendarData().then((res) =>
+      res.json()
+    );
+    setCalendarData(updatedCalendarData);
+  };
+
+  // Función para renderizar los eventos en el calendario
+  const tileContent = ({ date, view }: any) => {
+    if (view === "month" && calendarData) {
+      const event = calendarData.find((event: any) => {
+        const eventStartDate = new Date(format(event.start, "yyyy,MM,dd"));
+        return eventStartDate.toDateString() === date.toDateString();
       });
+      return event ? (
+        <p
+          className="p-1 hover:border  hover:bg-sky-500/40 transition rounded-xl"
+          onClick={() => handleModalOpen(event)}
+        >
+          {event.title}
+        </p>
+      ) : null;
     }
-    // let title = titleList;
-    // let calendarApi = selectInfo.view.calendar;
-    // console.log(selectInfo);
-    // const data2 = {
-    //   id: trainingLingId,
-    //   title: data.title,
-    //   start: selectInfo.startStr
-    // }
-    // /* createCalendarData(data2) */
-    // calendarApi.unselect(); // clear date selectio
-
-    // if (title) {
-    //   calendarApi.addEvent({
-    //     id: trainingLingId,
-    //     title: data.title,
-    //     start: selectInfo.startStr,
-    //     /* end: selectInfo.endStr,
-    //     allDay: selectInfo.allDay, */
-    //   });
-    // }
   };
 
-  let [currentMonth, setCurrentMonth] = useState<Date>(
-    startOfMonth(new Date())
-  );
-
-  const formatData = (x: any) => {
-    let newDate = x.replaceAll("-", ",");
-    return newDate;
-  };
-  /* const test = calendarData[0].start
-const test2 = test?.replaceAll("-", ",")
-console.log(test2)
-console.log(calendarData) */
-console.log(calendarData)
-
-const handleDayClick = (value:any) => {
-  const fechaOriginal = value
-  const fechaFormateada = format(fechaOriginal, "yyyy,MM,dd")
-  console.log(fechaOriginal)
-  console.log(fechaFormateada)
-  setOpen(true);
-};
-
-const events = [
-  { date: new Date(2024, 2, 10), title: 'Jhosbel' } // Fecha del evento: 10 de marzo de 2024
-];
-
-// Función para renderizar los eventos en el calendario
-const tileContent = ({ date, view }:any) => {
-  if (view === 'month' && calendarData) {
-    const event = calendarData.find((event:any) => {
-      const eventStartDate = new Date(formatData(event.start));
-      return eventStartDate.toDateString() === date.toDateString();
-    });
-    return event ? <p>{event.title}</p> : null;
-  }
-};
+//react-calendar__month-view__days
+//react-calendar__month-view__weekdays
+//react-calendar__tile react-calendar__month-view__days__day react-calendar__month-view__days__day--neighboringMonth
 
   return (
     <div className="absolute right-0 w-4/5 bg-slate-50">
       <h1>Proximamente</h1>
-      <div className="w-2/4 h-2/4">
-        {/* <FullCalendar
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          weekends={false}
-          eventClick={handleModalOpen}
-          locales={allLocales}
-          locale={"es"}
-          editable={true}
-          selectable={true}
-          select={handleDateSelect}
-          events={calendarData}
-        /> */}
+      <div className="w-full flex justify-center items-center">
+        <Calendar
+          onChange={onChange}
+          value={value}
+          onClickDay={handleDayClick}
+          tileContent={tileContent}
+          locale="es"
+        />
         <Modal isOpen={open} onClose={handleCloseModal}>
           {data && (
             <div key={data._id} className="bg-slate-200 flex w-full h-full">
@@ -216,16 +147,6 @@ const tileContent = ({ date, view }:any) => {
             ))}
           </div>
         )}
-      </div>
-      <div className="w-[500px] h-[500px]">
-        <h1>Otro Calendario</h1>
-        <Calendar
-          onChange={onChange}
-          value={value}
-          onClickDay={handleDayClick}
-          tileContent={tileContent}
-          locale="es"
-        />
       </div>
     </div>
   );
